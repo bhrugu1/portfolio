0f527f1fd752810a864eb836527d375dd55f9c93
bhrugu1 <braval5@myseneca.ca>
2025-07-25 14:13:35 -0400

    with vercel analytics added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
-
+import { Analytics } from "@vercel/analytics/react"
 import Header from './components/header';
 import Home from './components/Home';
 import About from './components/About'; 
